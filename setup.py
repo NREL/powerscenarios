74c8c1b52fb22c40a169e525db9a221421c4be31
Satkauskas <isatkaus@nrel.gov>
2020-07-02 14:47:44 -0600

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup                                                                                                            
+
+setup(name='powerscenarios',
+      version='0.1',
+      description='Renewable energy scenario generation for stochastic grid problems',
+      author='Ignas Satkauskas',
+      author_email='ignas.satkauskas@nrel.gov',
+      packages=['powerscenarios', ],
+      #package_data={'data': ['*.csv']}
+    )
+
