1161e00b8a543059456cdd7d6e353d185e704fef
Kinshuk Panda <kinshuk.panda@nrel.gov>
2021-07-26 16:41:28 -0600

    updates from a working abstracted checkmark model. Pyego pending

--- powerscenarios/costs/abstract_fidelity.py
+++ powerscenarios/costs/abstract_fidelity.py
@@ -7,5 +7,19 @@ import pandas as pd
 import os, sys, time
 
 class AbstractCostingFidelity(object):
-    def __init__(self):
-        pass
+    def __init__(self,
+                 n_scenarios,
+                 n_periods,
+                 loss_of_load_cost,
+                 spilled_wind_cost,
+                 scenarios_df,
+                 p_bin,
+                 WTK_DATA_PRECISION=6):
+
+        self.n_scenarios = n_scenarios
+        self.n_periods = n_periods
+        self.loss_of_load_cost = loss_of_load_cost
+        self.spilled_wind_cost = spilled_wind_cost
+        self.scenarios_df = scenarios_df
+        self.p_bin = p_bin
+        self.WTK_DATA_PRECISION=WTK_DATA_PRECISION
--- powerscenarios/costs/checkmark.py
+++ powerscenarios/costs/checkmark.py
@@ -5,6 +5,7 @@
 import numpy as np
 import pandas as pd
 import os, sys, time
+from powerscenarios.costs.abstract_fidelity import AbstractCostingFidelity
 
 class CheckmarkModel(AbstractCostingFidelity):
     """
@@ -12,23 +13,40 @@ class CheckmarkModel(AbstractCostingFidelity):
 
 
     """
-    def __init__(self, nscenarios):
-        AbstractCostingFidelity.__init__()
-        self.default_options_dict = {}
+    def __init__(self,
+                 n_scenarios, # Number of scenarios we actually want in our final csv file
+                 n_periods,
+                 loss_of_load_cost,
+                 spilled_wind_cost,
+                 scenarios_df,
+                 p_bin,
+                 WTK_DATA_PRECISION=6):
 
-    def compute_scenario_cost(self, reqired_arg_dict={}):
+        AbstractCostingFidelity.__init__(self,
+                                         n_scenarios,
+                                         n_periods,
+                                         loss_of_load_cost,
+                                         spilled_wind_cost,
+                                         scenarios_df,
+                                         p_bin,
+                                         WTK_DATA_PRECISION=6)
+
+
+    def compute_scenario_cost(self,
+                              random_seed=np.random.randint(2 ** 31 - 1)):
+        # compute costs of each 1-period scenario
+        self.cost_1 = self.scenarios_df["Deviation"].apply(
+            lambda dev: np.abs(self.loss_of_load_cost * dev)
+            if dev < 0
+            else np.abs(self.spilled_wind_cost * dev)
+        )
+        # print("any neg values in cost_1? {}".format((cost_1 < 0).any()))
+
         # based on n_periods, compute cost_n, rolling window sum (if n_periods is 1, this will be the same as cost_1)
-        cost_n = cost_1.rolling(n_periods).sum().shift(-(n_periods - 1))
+        self.cost_n = self.cost_1.rolling(self.n_periods).sum().shift(-(self.n_periods - 1))
         # rolling window operation looses digits, have to round (so we don't have negative values when adding zeroes)
-        cost_n = cost_n.round(self.WTK_DATA_PRECISION)
-        if (cost_n < 0).any():
-            print("any neg values in cost_n? {}".format((cost_n < 0).any()))
-        # IS
-        # probability mass function g(s) i.e. importance distribution
-        importance_probs = cost_n.loc[p_bin.index] / cost_n.loc[p_bin.index].sum()
+        self.cost_n = self.cost_n.round(self.WTK_DATA_PRECISION)
+        if (self.cost_n < 0).any():
+            print("any neg values in cost_n? {}".format((self.cost_n < 0).any()))
 
-        # sample of n_scenarios timestamps that are in wanted bin with probabilities given by cost_n series
-        sample_timestamps = p_bin.sample(
-            n_scenarios, random_state=random_seed, weights=importance_probs
-        ).index
-
+        return self.cost_n
